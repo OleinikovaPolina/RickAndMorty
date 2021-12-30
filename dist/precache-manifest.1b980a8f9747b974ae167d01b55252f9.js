ae417a714ea02587660725075ac998870722f349
OleinikovaPolina <90616548+OleinikovaPolina@users.noreply.github.com>
2021-12-30 14:06:16 +0300

    second

---
+++ dist/precache-manifest.1b980a8f9747b974ae167d01b55252f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9f142044bf2edea93a",
+    "url": "/css/chunk-vendors.d0ee0f91.css"
+  },
+  {
+    "revision": "fdcccc88a46e44dd94eb259497fbc770",
+    "url": "/img/3614168-total_rickall.fdcccc88.jpg"
+  },
+  {
+    "revision": "2a2c98d28dca4a0d133e737d75c509b3",
+    "url": "/img/Multiverse.2a2c98d2.jpg"
+  },
+  {
+    "revision": "82b8707ff5c6b7fa380b2cdf44a4c21a",
+    "url": "/img/rick.82b8707f.jpg"
+  },
+  {
+    "revision": "fcd7615a86d481d783396fb6ade1319b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9421d66417699e736a5b",
+    "url": "/js/app.792c566a.js"
+  },
+  {
+    "revision": "2e9f142044bf2edea93a",
+    "url": "/js/chunk-vendors.03341064.js"
+  },
+  {
+    "revision": "298c4633bdfab5ee686a4c7057fffa3d",
+    "url": "/logo192.png"
+  },
+  {
+    "revision": "f9c6757e904914175223eaecababfbd4",
+    "url": "/logo512.png"
+  },
+  {
+    "revision": "b55d0558ad49ba1895e7c586211dd12e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
